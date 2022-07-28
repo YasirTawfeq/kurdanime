e842802b620870077b7adeffc17f505238845758
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-07-28 19:05:09 +0300

    search enabled and some design changed

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -9,9 +9,7 @@ function NotFound() {
 		Page Not Found
 	</div>
 	<button className="mt-5">
-      <a
-        className="relative inline-block text-sm font-medium text-yellow-300 group active:text-yellow-300 focus:outline-none focus:ring"
-      >
+
         <span
           className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-yellow-300 group-hover:translate-y-0 group-hover:translate-x-0"
         ></span>
@@ -19,7 +17,6 @@ function NotFound() {
         <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
           <Link to="/">Go Home</Link>
         </span>
-      </a>
     </button>
 </div>
   )
