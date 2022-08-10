8656421b6a3f538cf7b7764e6aaa96d27c963ff2
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-08-10 11:48:22 +0300

    fixing bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/Components/*"],
+ content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
